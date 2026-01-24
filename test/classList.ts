cb552d26f5277e1ebc5358adbd1db0d8d6ea48c2
Lukas Obermann <lukas.obermann@icloud.com>
2026-01-24 15:22:20 +0100

    feat: add classlist function

---
+++ test/classList.ts
@@ -0,0 +1,16 @@
+import assert from "node:assert/strict"
+import { describe, it } from "node:test"
+import { classList } from "../src/classList.js"
+
+describe("classList", () => {
+  it("returns a string of class names from the given arguments", () => {
+    assert.equal(
+      classList("foo", "bar", undefined, null, {
+        baz: true,
+        no: false,
+        alsoNo: undefined,
+      }),
+      "foo bar baz",
+    )
+  })
+})
