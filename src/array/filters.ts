8b49fffeaabe30dedb0015f2f94ae80737c4b210
Lukas Obermann <lukas.obermann@icloud.com>
2026-01-24 18:50:20 +0100

    feat: add function to identify duplicates in arrays

--- src/array/filters.ts
+++ src/array/filters.ts
@@ -7,3 +7,35 @@ export const unique = <
 >(
   arr: T[],
 ): T[] => Array.from(new Set(arr))
+
+/**
+ * Checks if there are any duplicate elements in the array.
+ */
+export const anySame = <T>(
+  arr: T[],
+  equalityCheck: (a: T, b: T) => boolean = (a, b) => a === b,
+) =>
+  arr.some(
+    (item, index) =>
+      arr.findIndex((other) => equalityCheck(item, other)) !== index,
+  )
+
+/**
+ * Checks if there are any duplicate elements in the array and returns an array
+ * of found duplicates where the values are the indices of these values.
+ */
+export const anySameIndices = <T>(
+  arr: T[],
+  equalityCheck: (a: T, b: T) => boolean = (a, b) => a === b,
+): number[][] =>
+  arr.reduce((acc: number[][], item, index) => {
+    const firstIndex = arr.findIndex((other) => equalityCheck(item, other))
+    if (firstIndex === index) {
+      return acc
+    }
+    const accIndex = acc.findIndex((accElem) => accElem[0] === firstIndex)
+    return accIndex === -1
+      ? [...acc, [firstIndex, index]]
+      : // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- The index must exist according to the findIndex above
+        acc.with(accIndex, [...acc[accIndex]!, index])
+  }, [])
--- test/array/filters.ts
+++ test/array/filters.ts
@@ -1,11 +1,48 @@
 import { deepEqual } from "node:assert/strict"
 import { describe, it } from "node:test"
-import { unique } from "../../src/array/filters.js"
+import { anySame, anySameIndices, unique } from "../../src/array/filters.js"
 
 describe("unique", () => {
   it("filters out duplicate values from an array", () => {
     deepEqual(unique([]), [])
     deepEqual(unique([1, 2, 3]), [1, 2, 3])
     deepEqual(unique([1, 2, 1, 3]), [1, 2, 3])
+  })
+})
+
+describe("anySame", () => {
+  it("returns false when given an empty array", () => {
+    deepEqual(anySame([]), false)
+  })
+
+  it("checks for duplicate values from the array using shallow equality checks", () => {
+    deepEqual(anySame([1, 2, 2, 3, 4, 4]), true)
+  })
+
+  it("checks for duplicate values from the array using custom equality checks", () => {
+    const arr = [{ id: 1 }, { id: 2 }, { id: 1 }]
+    const equalityCheck = (a: { id: number }, b: { id: number }) =>
+      a.id === b.id
+    deepEqual(anySame(arr, equalityCheck), true)
+  })
+})
+
+describe("anySameIndices", () => {
+  it("returns an empty array when given an empty array", () => {
+    deepEqual(anySameIndices([]), [])
+  })
+
+  it("checks for duplicate values from the array using shallow equality checks", () => {
+    deepEqual(anySameIndices([1, 2, 2, 3, 4, 4]), [
+      [1, 2],
+      [4, 5],
+    ])
+  })
+
+  it("checks for duplicate values from the array using custom equality checks", () => {
+    const arr = [{ id: 1 }, { id: 2 }, { id: 1 }]
+    const equalityCheck = (a: { id: number }, b: { id: number }) =>
+      a.id === b.id
+    deepEqual(anySameIndices(arr, equalityCheck), [[0, 2]])
   })
 })
