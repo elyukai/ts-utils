00408edcbfa6b18fb20eede78c5f19efbe07fcd1
Lukas Obermann <lukas.obermann@icloud.com>
2026-02-20 22:54:31 +0100

    feat: add more reader methods
    Add functions for modifying and widening the environment.
    Add sequencing operation where the reader and its wrapper type are
    flipped (array of readers becomes reader of array; function resulting in
    reader becomes a reader that returns a function).

--- src/reader.ts
+++ src/reader.ts
@@ -3,12 +3,13 @@
  * @module
  */
 
+// A simpler wrapper for working with text that is generated based on different contexts, which encapsulates the context and reduces main code clutter. The context is only applied in the end.
 /**
  * The Reader monad represents a computation that depends on some shared environment. It allows you to write functions that can access this environment without having to pass it explicitly as an argument.
  * @template T The type of the value produced by the Reader.
  * @template R The type of the environment that the Reader depends on.
  */
-export class Reader<T, R> {
+export class Reader<in R, out T> {
   readonly #fn: (env: R) => T
 
   private constructor(fn: (env: R) => T) {
@@ -18,7 +19,7 @@ export class Reader<T, R> {
   /**
    * Always produce the given value, regardless of the environment.
    */
-  static of<T, R>(value: T): Reader<T, R> {
+  static of<R = unknown, T = never>(value: T): Reader<R, T> {
     return new Reader(() => value)
   }
 
@@ -32,22 +33,47 @@ export class Reader<T, R> {
   /**
    * Retrieve a transformed value of the environment.
    */
-  static asks<T, R>(f: (env: R) => T): Reader<T, R> {
+  static asks<R, T>(f: (env: R) => T): Reader<R, T> {
     return new Reader((env) => f(env))
   }
 
   /**
    * Applies the given function to the value produced by this Reader and returns a new Reader that produces the result.
    */
-  map<U>(f: (value: T) => U): Reader<U, R> {
+  map<U>(f: (value: T) => U): Reader<R, U> {
     return new Reader((env) => f(this.#fn(env)))
+  }
+
+  /**
+   * Combines this Reader with another Reader by applying a function to both of their results and returning a new Reader that produces the result.
+   */
+  map2<U, V>(other: Reader<R, U>, f: (first: T, second: U) => V): Reader<R, V> {
+    return new Reader((env) => f(this.#fn(env), other.#fn(env)))
   }
 
   /**
    * Applies the given function to the value produced by this Reader and returns a new Reader that produces the result. The function must return a Reader, which allows you to chain computations that depend on the same environment.
    */
-  then<U>(f: (value: T) => Reader<U, R>): Reader<U, R> {
+  then<U>(f: (value: T) => Reader<R, U>): Reader<R, U> {
     return new Reader((env) => f(this.#fn(env)).#fn(env))
+  }
+
+  /**
+   * Applies the given function to the value produced by this Reader and returns a new Reader that produces the result. The function must return a Reader, which allows you to chain computations that depend on a shared environment. The environments do not have to be the same, but will need to be combined.
+   *
+   * The suffix ‘W’ stands for ‘widening’, as this method allows you to widen the environment that the Reader depends on.
+   */
+  thenW<U, S>(f: (value: T) => Reader<S, U>): Reader<R & S, U> {
+    return new Reader((env) => f(this.#fn(env)).#fn(env))
+  }
+
+  /**
+   * Modifies the environment for this Reader by applying the given function to it before running the computation.
+   *
+   * This allows you to adapt the environment for a specific computation without changing the original Reader.
+   */
+  with<S>(modifyEnv: (env: S) => R): Reader<S, T> {
+    return new Reader((env) => this.#fn(modifyEnv(env)))
   }
 
   /**
@@ -56,4 +82,42 @@ export class Reader<T, R> {
   run(env: R): T {
     return this.#fn(env)
   }
+
+  /**
+   * Evaluate each reader in the array, and collect the results in a single reader instance.
+   */
+  static sequence<R, T>(readers: Reader<R, T>[]): Reader<R, T[]> {
+    return new Reader((env) => readers.map((reader) => reader.run(env)))
+  }
+
+  /**
+   * Transforms a function that returns a Reader into a Reader that produces a function.
+   *
+   * This makes functions producing Readers that depend on more arguments sometimes easier to work with.
+   */
+  static defer<Args extends unknown[], R, T>(
+    fn: (...args: Args) => Reader<R, T>,
+  ): Reader<R, (...args: Args) => T> {
+    return new Reader(
+      (env) =>
+        (...args: Args) =>
+          fn(...args).run(env),
+    )
+  }
+
+  /**
+   * Transforms a Reader that produces a function into a function that returns a Reader.
+   *
+   * This is the inverse of {@link defer}.
+   */
+  static undefer<Args extends unknown[], R, T>(
+    reader: Reader<R, (...args: Args) => T>,
+  ): (...args: Args) => Reader<R, T> {
+    return (...args: Args) => new Reader((env) => reader.run(env)(...args))
+  }
 }
+
+// type Readable<T> =
+//   T extends Record<string, unknown>
+//     ? { [K in keyof T]: Readable<T[K]> }
+//     : T
--- test/reader.ts
+++ test/reader.ts
@@ -39,3 +39,57 @@ describe("then", () => {
     equal(reader.run({ foo: "bar" }), 3)
   })
 })
+
+describe("thenW", () => {
+  it("chains computations that depend on a shared environment", () => {
+    const reader = Reader.ask<{ foo: string }>().thenW((env) =>
+      Reader.asks((otherEnv: { bar: number }) => env.foo.length + otherEnv.bar),
+    )
+    equal(reader.run({ foo: "bar", bar: 2 }), 5)
+  })
+})
+
+describe("with", () => {
+  it("modifies the environment for a specific computation", () => {
+    const reader = Reader.ask<{ foo: string }>().with(
+      (env: { foo: string }) => ({
+        ...env,
+        foo: env.foo.toUpperCase(),
+      }),
+    )
+    deepEqual(reader.run({ foo: "bar" }), { foo: "BAR" })
+  })
+})
+
+describe("sequence", () => {
+  it("transforms an array of Readers into a Reader that produces an array of their results", () => {
+    const readers: Reader<{ foo: string; bar: number }, string | number>[] = [
+      Reader.asks((env: { foo: string }) => env.foo),
+      Reader.asks((env: { bar: number }) => env.bar),
+    ]
+    const reader = Reader.sequence(readers)
+    deepEqual(reader.run({ foo: "hello", bar: 42 }), ["hello", 42])
+  })
+})
+
+describe("defer", () => {
+  it("transforms a function that returns a Reader into a Reader that produces a function", () => {
+    const reader = Reader.defer((x: number) =>
+      Reader.asks((env: { multiplier: number }) => x * env.multiplier),
+    )
+    const multiplyBy2 = reader.run({ multiplier: 2 })
+    equal(multiplyBy2(3), 6)
+  })
+})
+
+describe("undefer", () => {
+  it("transforms a Reader that produces a function into a function that returns a Reader", () => {
+    const reader = Reader.undefer(
+      Reader.asks(
+        (env: { multiplier: number }) => (x: number) => x * env.multiplier,
+      ),
+    )
+    const multiplyBy2 = reader(2)
+    equal(multiplyBy2.run({ multiplier: 3 }), 6)
+  })
+})
