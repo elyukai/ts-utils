5a97a69f25ac8431d3bc29405b312f750f0e30b9
Lukas Obermann <lukas.obermann@icloud.com>
2026-02-24 13:56:56 +0100

    feat: safe non-empty array map method call

--- src/array/nonEmpty.ts
+++ src/array/nonEmpty.ts
@@ -13,6 +13,21 @@ export type EmptyArray = []
  */
 export type NonEmptyArray<T> = [T, ...T[]]
 
+declare global {
+  interface Array<T> {
+    /**
+     * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+     * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
+    map<U>(
+      this: NonEmptyArray<T>,
+      callbackfn: (value: T, index: number, array: T[]) => U,
+      thisArg?: unknown,
+    ): NonEmptyArray<U>
+  }
+}
+
 /**
  * Checks if the array is empty.
  */
